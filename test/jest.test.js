0a9d8534f5d3b65d655518c0dbe95b6ece362fc9
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-07-04 11:36:57 -0400

    a

--- test/jest.test.js
+++ test/jest.test.js
@@ -8,3 +8,15 @@ test('should be able to know somethings about jest', () => {
   expect(number).toBeGreaterThan(9);
   expect(number).toBeLessThan(13);
 });
+
+test('Devo saber trabalhar com Objeto', () => {
+  const obj = { nome: 'weslley', email: 'weslley@gmail.com' };
+  expect(obj).toHaveProperty('nome');
+  expect(obj).toHaveProperty('nome', 'weslley');
+  expect(obj.nome).toBe('weslley');
+
+  const obj2 = { nome: 'weslley', email: 'weslley@gmail.com' };
+
+  expect(obj2).toEqual(obj);
+  expect(obj2).toBe(obj2);
+});
